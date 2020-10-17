0fe599bdc4909b0a0462f118d1e16ca20bb7c071
Triangle TARDIS <triangletardis@gmail.com>
2020-10-16 23:53:56 -0400

    Terminal to 30 lines

--- go_auto.sh
+++ go_auto.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ~pi/TARDIS-Pi
-sh console_init.sh
-lxterminal --working-directory=~pi/TARDIS-Pi --geometry=80x33 -e "./console.py auto"
+./console_init.sh
+lxterminal --geometry=80x30 -e "./console.py auto"
+sleep 2
 
